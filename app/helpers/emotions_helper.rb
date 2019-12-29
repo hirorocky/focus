3947bc471072047b0b640eb15b30ff9ea576b805
hirorocky <hirorocky@gmail.com>
2019-12-29 19:29:41 +0900

    mod emotion#index text-color(close #13)

--- app/helpers/emotions_helper.rb
+++ app/helpers/emotions_helper.rb
@@ -1,2 +1,21 @@
+# frozen_string_literal: true
+
 module EmotionsHelper
+  def emotion_text_color(color)
+    brightness = color.scan(/\w\w/).map(&:hex).max
+    if brightness > 127
+      'black'
+    else
+      'white'
+    end
+  end
+
+  def emotion_text_color_class(color)
+    brightness = color.scan(/\w\w/).map(&:hex).max
+    if brightness > 127
+      'has-text-black'
+    else
+      'has-text-white'
+    end
+  end
 end
