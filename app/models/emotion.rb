ea1817af72cca755539e6daab94a4c5f6d884b4a
hirorocky <hirorocky@gmail.com>
2019-12-29 15:27:26 +0900

    add destroy dependent tot emotion (close #10)

--- app/models/emotion.rb
+++ app/models/emotion.rb
@@ -1,27 +1,29 @@
+# frozen_string_literal: true
+
 class Emotion < ApplicationRecord
   before_save :upcase_color
   belongs_to :user
-  has_many :fragments
+  has_many :fragments, dependent: :destroy
   has_many :tags, through: :fragments
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :color, presence: true, format: { with: /\A#[a-f\d]{6}/i }
 
   def upcase_color
-    self.color.upcase!
+    color.upcase!
   end
 
   def created_at_with_format
-    self.created_at.strftime('%Y/%m/%d %H:%M:%S')
+    created_at.strftime('%Y/%m/%d %H:%M:%S')
   end
 
   def fragments_tag_content_pairs
     fragments = self.fragments
     result = []
     fragments.each do |fragment|
-      tag_name = self.tags.find(fragment.tag_id).name
+      tag_name = tags.find(fragment.tag_id).name
       content = fragment.content
-      result << {tag: tag_name, content: content}
+      result << { tag: tag_name, content: content }
     end
     result
   end
